919fc4ffdd6353681380422e2340fef7c3306877
MORIMORI0317 <morimori.0317@outlook.jp>
2021-12-25 15:27:56 +0900

    1.24

--- src/main/java/dev/felnull/fnjl/util/FNImageUtil.java
+++ src/main/java/dev/felnull/fnjl/util/FNImageUtil.java
@@ -105,9 +105,9 @@ public class FNImageUtil {
      */
     public static boolean isImage(byte[] data) {
         try {
-            ImageIO.read(new ByteArrayInputStream(data));
+            ImageIO.read(new ByteArrayInputStream(data)).getType();
             return true;
-        } catch (IOException e) {
+        } catch (IOException | NullPointerException e) {
             return false;
         }
     }
@@ -121,18 +121,31 @@ public class FNImageUtil {
      * @throws IOException exception
      */
     public static BufferedImage reductionSize(BufferedImage image, long size) throws IOException {
-        long lastSize = toByteArray(image, "png").length;
+        return reductionSize(image, size, "png");
+    }
+
+    /**
+     * 画像を指定サイズまで縮小する
+     *
+     * @param image      対象画像
+     * @param size       サイズ
+     * @param formatName フォーマット名
+     * @return 縮小済み
+     * @throws IOException exception
+     */
+    public static BufferedImage reductionSize(BufferedImage image, long size, String formatName) throws IOException {
+        long lastSize = toByteArray(image, formatName).length;
         if (lastSize <= size) return image;
         float scale = (float) size / lastSize;
         BufferedImage nimg = resize(image, (int) (image.getWidth() * scale), (int) (image.getHeight() * scale));
-        return reductionSizeW(nimg, size, lastSize);
+        return reductionSizeW(nimg, size, lastSize, formatName);
     }
 
-    private static BufferedImage reductionSizeW(BufferedImage image, long size, long lastSize) throws IOException {
-        byte[] data = toByteArray(image, "png");
+    private static BufferedImage reductionSizeW(BufferedImage image, long size, long lastSize, String formatName) throws IOException {
+        byte[] data = toByteArray(image, formatName);
         if (data.length == lastSize) throw new IOException("Can't be smaller than this");
         if (data.length <= size) return image;
         BufferedImage nimg = resize(image, image.getWidth() / 2, image.getHeight() / 2);
-        return reductionSizeW(nimg, size, data.length);
+        return reductionSizeW(nimg, size, data.length, formatName);
     }
 }
--- src/test/java/dev/felnull/TestMain.java
+++ src/test/java/dev/felnull/TestMain.java
@@ -1,17 +1,19 @@
 package dev.felnull;
 
+import dev.felnull.fnjl.util.FNDataUtil;
 import dev.felnull.fnjl.util.FNImageUtil;
 import dev.felnull.fnjl.util.FNStringUtil;
+import dev.felnull.fnjl.util.FNURLUtil;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.net.URL;
 
 public class TestMain {
     public static void main(String[] args) throws Exception {
         System.out.println(FNStringUtil.getByteDisplay(1000 * 1000));
-        BufferedImage img = FNImageUtil.reductionSize(ImageIO.read(new File("V:\\dev\\java\\FelNullJavaLibrary\\test\\t.png")), 1000 * 1000);
-        ImageIO.write(img, "png", new File("V:\\dev\\java\\FelNullJavaLibrary\\test\\t2.png"));
+        //   BufferedImage img = FNImageUtil.reductionSize(ImageIO.read(new File("V:\\dev\\java\\FelNullJavaLibrary\\test\\t.png")), 1000 * 1000);
+        //   ImageIO.write(img, "png", new File("V:\\dev\\java\\FelNullJavaLibrary\\test\\t2.png"));
+        boolean im = FNImageUtil.isImage(FNDataUtil.streamToByteArray(FNURLUtil.getStream(new URL("https://pbs.twimg.com/media/FHXlg_QacAMfISG?format=png&name=medium"))));
+        System.out.println(im);
         //System.out.println(FNMath.scale(512, 1024));
       /*  long time = System.currentTimeMillis();
         FNRuntimeUtil.multipleRun("Ikisugi", 2, () -> {
